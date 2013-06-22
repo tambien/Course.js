77f95488aa78219fa611eb3fef7a6ad7a64293c7
Yotam Mann <yotammann@gmail.com>
2013-06-22 14:21:46 -0400

    removed inline script from first.html

---
+++ scripts/application/jQuerying.js
@@ -0,0 +1,8 @@
+$(function(){
+	var redBackgrounds = $(".redBackground");
+	redBackgrounds.html("<div>here is new html</div>");
+	redBackgrounds.css({
+		"background-color"  : "#00ff00"
+	})
+})
+ 	 
