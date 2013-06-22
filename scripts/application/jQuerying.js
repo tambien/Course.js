305a5f1f4474a306d2f6a129a91cd71b53d8117c
Yotam Mann <yotammann@gmail.com>
2013-06-22 16:04:11 -0400

    random string in the div box

--- scripts/application/jQuerying.js
+++ scripts/application/jQuerying.js
@@ -1,8 +1,17 @@
+//START HERE
 $(function(){
-	var redBackgrounds = $(".redBackground");
-	redBackgrounds.html("<div>here is new html</div>");
-	redBackgrounds.css({
-		"background-color"  : "#00ff00"
-	})
-})
- 	 
+	console.log("jQuerying application running");
+	setInterval(loop, 1000);
+});
+
+var sourceText = "A written language is the representation of a language by means of a writing system. Written language is an invention in that it must be taught to children; children will pick up spoken language (oral or sign) by exposure without being specifically taught. A written language exists only as a complement to a specific spoken language, and no natural language is purely written. However, extinct languages may be in effect purely written when only their writings survive. [citation needed]";
+
+function loop(){
+	//breaking up my source text into an array
+	var brokenText = sourceText.split(" ");
+	//choose a random spot in my array
+	var randomSpot = parseInt(Math.random() * sourceText.length);
+	var randomWord = brokenText[randomSpot];
+	//asign contents of #notTextBox to randomWord
+	$("#notTextBox").html(randomWord);
+}
