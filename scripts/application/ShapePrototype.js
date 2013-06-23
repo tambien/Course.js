be21b6efd592670520b7e6bf3f55852868cd6556
Yotam Mann <yotammann@gmail.com>
2013-06-23 14:31:07 -0400

    added static incrementing to ShapeCount

--- scripts/application/ShapePrototype.js
+++ scripts/application/ShapePrototype.js
@@ -1,4 +1,6 @@
 var Shape = function(){
+
+	//instance variables
 	this.x = -1;
 	this.y = -1;
 
@@ -7,10 +9,16 @@ var Shape = function(){
 		b : 255,
 		g : 0
 	}
+
+	Shape.prototype.shapeCount++;
 }
 
+//static variable
 Shape.prototype.name = "shape";
 
+Shape.prototype.shapeCount = 0;
+
+//class method
 Shape.prototype.move = function(x, y){
 	this.x = x;
 	this.y = y;
